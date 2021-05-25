b840fc0c7e4cbfbedf258b67efcadb4345df867e
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-05-25 22:04:22 +0530

    add list collections api

---
+++ src/Unsplash.Tests/CollectionsApiTests.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Unsplash.Api.Collections;
+using Unsplash.Client;
+using Unsplash.Models;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using Xunit;
+
+namespace Unsplash.Tests
+{
+    public class CollectionsApiTests : IDisposable
+    {
+        private readonly WireMockServer _server;
+        private const string ACCESS_KEY = "Token";
+
+        public CollectionsApiTests()
+        {
+            _server = WireMockServer.Start();
+        }
+
+        [Fact]
+        public async Task ListCollections()
+        {
+            var collectionsData = JsonConvert.DeserializeObject<IEnumerable<CollectionBasic>>(await File.ReadAllTextAsync("data/collections/ListCollectionsResponse.json"));
+            var jsonData = JsonConvert.SerializeObject(collectionsData);
+
+            _server.Given(
+                WireMockHelpers.CreateGetRequestBuilder(
+                    CollectionsApiUrls.List(),
+                    ACCESS_KEY,
+                    Constants.API_VERSION
+                )
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithBody(jsonData)
+            );
+
+            var client = new CollectionsApi(new ApiClientOptions
+            {
+                BaseUrl = _server.Urls[0],
+                AccessKey = ACCESS_KEY
+            });
+
+            var response = await client.ListAsync();
+
+            var responseJson = JsonConvert.SerializeObject(response);
+
+            Assert.Equal(jsonData, responseJson);
+        }
+
+        public void Dispose()
+        {
+            _server.Stop();
+            _server.Dispose();
+        }
+    }
+}
---
+++ src/Unsplash/Api/Collections/CollectionsApi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Unsplash.Client;
+using Unsplash.Models;
+
+namespace Unsplash.Api.Collections
+{
+    public interface ICollectionsApi
+    {
+        Task<IEnumerable<CollectionBasic>> ListAsync(ListCollectionsParams parameters = null);
+    }
+
+    public static class CollectionsApiUrls
+    {
+        public static string List() => "/collections";
+    }
+
+    public class ListCollectionsParams
+    {
+        public ListCollectionsParams(int? page, int? perPage)
+        {
+            Page = page;
+            PerPage = perPage;
+        }
+
+        public static ListCollectionsParams Default => new ListCollectionsParams(1, 10);
+
+        public int? Page { get; }
+        public int? PerPage { get; }
+    }
+
+    public class CollectionsApi : ApiClient, ICollectionsApi
+    {
+        public CollectionsApi(ApiClientOptions options) : base(options)
+        {
+        }
+
+        public async Task<IEnumerable<CollectionBasic>> ListAsync(ListCollectionsParams parameters = null)
+        {
+            if (parameters == null)
+            {
+                parameters = ListCollectionsParams.Default;
+            }
+
+            var queryParams = new Dictionary<string, string>()
+            {
+                { "page", parameters.Page?.ToString() },
+                { "per_page", parameters.PerPage?.ToString() }
+            };
+
+            var url = $"{CollectionsApiUrls.List()}?{UrlHelper.CreateQueryString(queryParams)}";
+
+            return await GetAsync<IEnumerable<CollectionBasic>>(url);
+        }
+    }
+}
